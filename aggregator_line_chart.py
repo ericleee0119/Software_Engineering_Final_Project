e670bb3b21f5720ca2a5eb1231cabd483754e33c
Chanpy-J <cc7283@nyu.edu>
2023-05-04 16:31:33 -0400

    create line plot structure

---
+++ aggregator_line_chart.py
@@ -0,0 +1,9 @@
+from plot_aggregator import aggregator
+import abc
+
+class line_chart(aggregator):
+	def __init__():
+
+	@abc.abstractmethod
+	def draw():
+		
---
+++ plot_aggregator.py
@@ -0,0 +1,11 @@
+import abc
+class aggregator(abc.ABC):
+
+	def __init__(self):
+
+
+
+	@abc.abstractmethod
+	def draw():
+
+
