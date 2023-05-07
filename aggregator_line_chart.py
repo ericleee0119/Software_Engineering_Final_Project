b288fa88a022ee277b5b45a2778339d6dbc4de35
Chanpy-J <cc7283@nyu.edu>
2023-05-07 02:43:05 -0400

    new plot structure

--- aggregator_line_chart.py
+++
@@ -1,8 +0,0 @@
-from plot_aggregator import aggregator
-from abc import ABC
-
-class line_chart(aggregator):
-	def __init__():
-
-	@abc.abstractmethod
-	def draw():
---
+++ registry.py
@@ -0,0 +1,7 @@
+import line_chart
+import histogram_plot
+
+PlotRegistry= {
+	'line_plot' : line_chart,
+	'histogram_plot' : histogram_plot
+}
