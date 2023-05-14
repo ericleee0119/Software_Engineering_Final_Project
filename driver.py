51929a4527b10a172a2dd955b458198ae593c8e8
ericleee0119 <50438750+ericleee0119@users.noreply.github.com>
2023-05-13 21:32:20 -0400

    Update map

--- driver.py
+++ driver.py
@@ -1,86 +1,10 @@
-import seaborn as sns 
-import matplotlib.pyplot as plt 
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
 import pandas as pd
-import webbrowser
-import os
-#import gmplot
-import plot_Factory as PF
-
-import finalproject_UI
-
-#command: choose which plot to use -> UI -> call map
-#plot name, line chart, histgram, map 2. data 3. UI chosen col, city, time, month
-#call which plot -> (diff plots have diff UI)
-
-#data = None
-
-def Data_preprocess(data):
-    data_mod = data
-    data_mod['CMPLNT_FR_TM'] = data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
-    data_mod['month'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[0]
-    data_mod['CMPLNT_FR_DT'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[2]
+import map
 
 
-    #print(data_mod)
-    data_mod = data_mod.loc[(data_mod['CMPLNT_FR_DT'] == '2012') | (data_mod['CMPLNT_FR_DT'] == '2013') |
-                            (data_mod['CMPLNT_FR_DT'] == '2014') | (data_mod['CMPLNT_FR_DT'] == '2015') |
-                            (data_mod['CMPLNT_FR_DT'] == '2016') | (data_mod['CMPLNT_FR_DT'] == '2017') |
-                            (data_mod['CMPLNT_FR_DT'] == '2018') | (data_mod['CMPLNT_FR_DT'] == '2019') |
-                            (data_mod['CMPLNT_FR_DT'] == '2020') | (data_mod['CMPLNT_FR_DT'] == '2021')]
-    return data_mod
+# map_data = pd.read_csv('NYPD_Complaint_Data_Current__Year_To_Date_1.csv')
+map_data = pd.read_csv('NYPD_Complaint_Data_Historic.csv')
+mapPlot = map.MapPlot()
 
-def File_input():
-    global data
-    file_name = input("csvFileName: ")
-    try:
-        data = pd.read_csv(file_name)
-        data = Data_preprocess(data)
-    except Exception as e:
-        print(f"Error: {e}")
-    
-
-def Data_plot():
-    # if not data:
-    #     print("You need to inpu data file first, use File_input commend")
-    #     return
-
-    selected_options = finalproject_UI.UI()
-    print("These are the elements:", end=".")
-    print(*selected_options, sep=", ")
-    
-    #hard coding part
-    chosen_city = selected_options[0]
-    chosen_plot = selected_options[1]
-    chosen_col = selected_options[2]
-    print(chosen_city, chosen_plot, chosen_col)
-
-    plot1 = PF.plot_Factory.create(chosen_plot, data)
-    plot1.draw(chosen_col)
-
-
-commands = {
-    "File_input": File_input,
-    "Data_plot": Data_plot,
-    "help": lambda: print("Available commands: help, quit, File_input, Data_plot")
-}
-
-def main():
-    print("Welcome to the command line interface")
-    while True:
-        command = input("Enter a command: ")
-        if command == "quit":
-            print("Exiting...")
-            break
-        elif command in commands:
-            func = commands[command]
-            try:
-                result = func()
-                print(f"Result: {result}")
-            except ValueError as e:
-                print(f"Error: {e}")
-        else:
-            print("Invalid command. Type 'help' for a list of available commands.")
-
-main()
+mapPlot.create(map_data)
+mapPlot.draw()
--- map.py
+++ map.py
@@ -16,6 +16,10 @@ buttonClick = False
 class MapPlot():
 
         map_data_mod = {}
+        city = ""
+        slider = ""
+        pin = ""
+        api = ""
         slider = widgets.IntSlider(value = 30, min = 5, max = 50, step = 5, 
                               description = "clusters num", continuous_update=False, readout = True)
         def create(self, map_data):
@@ -25,18 +29,22 @@ class MapPlot():
             self.map_data_mod['CMPLNT_FR_TM'] = self.map_data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
             self.map_data_mod['CMPLNT_FR_DT'] = self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] + '/' + self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[2]
 
-            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[1] == '2022')]
-            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] == '09')]     
+            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[1] == '2021')]
+            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] == '08')]     
             
-        
+            print(self.map_data_mod)
         
         def draw(self):
             def setbuttonclick(b, city="", slider = "", pin="", api=""):
-                print(city)
-                buttonClick = True
-                  
-            #def draw_map(city, pin, slider, api):
-
+                city = self.city
+                slider = self.slider
+                pin = self.pin
+                api = self.api
+                global buttonClick
+                if api == "":
+                    buttonClick = True
+                    return
+              
                 curr_data = self.map_data_mod
                 first_place = "New York, USA"
                 if city != 'ALL':
@@ -45,7 +53,6 @@ class MapPlot():
                 min_num = len(curr_data)
                 min_num = min(1000, len(curr_data))
                 curr_data = curr_data.sample(n = min_num, replace = False)
-
                 coords = np.array(curr_data[['Latitude', 'Longitude']], dtype='float64')
                 mask = np.any(np.isnan(coords) | np.equal(coords, 0), axis=1)
                 coords[~mask]
@@ -57,6 +64,7 @@ class MapPlot():
                 up = slider
                 Ks = range(1, up)
                 algorithmFactory = AlgorithmFactory.AlgorithmFactory.create("kmean")
+                
                 kmean = algorithmFactory.calculate(up, Ks, coords)
                 lat_list = []
                 long_list = []
@@ -64,18 +72,16 @@ class MapPlot():
                 for i in range(len(kmean)):
                     lat_list.append(kmean[up - 2].cluster_centers_[i][0])
                     long_list.append(kmean[up - 2].cluster_centers_[i][1])
-                print(api)
+
                 apikey = str(api)
-                #gmap = gmplot.GoogleMapPlotter.from_geocode("New York, USA", apikey=apikey)
-                gmap = gmplot.GoogleMapPlotter.from_geocode(first_place, apikey=apikey)
-                # gmap.heatmap( lat_list, long_list, radius = 15)
+                gmap = gmplot.GoogleMapPlotter.from_geocode("New York, USA", apikey=apikey)
+
                 if pin == "HEAT":
                     gmap.heatmap( lat_list, long_list, radius = 15)
                 else:
                     gmap.scatter(lat_list, long_list, "cornflowerblue")
                 gmap.draw('map.html')
 
-                #f=codecs.open("map.html", 'r')
                 filename = 'file:///'+os.getcwd()+'/' + 'map.html'
                 webbrowser.open_new_tab(filename)
                 buttonClick = False
@@ -83,8 +89,12 @@ class MapPlot():
             def map_plot(city = (['ALL', 'BRONX', 'BROOKLYN', 'MANHATTAN', 'QUEENS', 'STATEN ISLAND']), 
                                       pin = (['HEAT', 'SCATTER']), 
                                        slider = self.slider, api = api):
+                self.city = city
+                self.slider = slider
+                self.pin = pin
+                self.api = api
                 display(button)
-                button.on_click(functools.partial(setbuttonclick, city=city, slider = slider, pin=pin, api=api))
+            button.on_click(functools.partial(setbuttonclick, city=self.city, slider = self.slider, pin=self.pin, api=self.api))
                 
                 
                 
