15f5d06d9a92cc434c867c429f62b9b2dfa74030
Chanpy-J <cc7283@nyu.edu>
2023-05-18 17:53:01 -0400

    mod prep

--- test_plot.py
+++ test_plot.py
@@ -4,23 +4,29 @@ from registry import ColRegistry
 
 data = pd.read_csv('test_data.csv')
 data_mod = data
+'''
 data_mod['CMPLNT_FR_TM'] = data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
 data_mod['month'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[0]
 data_mod['CMPLNT_FR_DT'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[2]
 
-#print(data_mod)
+
+
 data_mod = data_mod.loc[(data_mod['CMPLNT_FR_DT'] == '2012') | (data_mod['CMPLNT_FR_DT'] == '2013') |
                         (data_mod['CMPLNT_FR_DT'] == '2014') | (data_mod['CMPLNT_FR_DT'] == '2015') |
                         (data_mod['CMPLNT_FR_DT'] == '2016') | (data_mod['CMPLNT_FR_DT'] == '2017') |
                         (data_mod['CMPLNT_FR_DT'] == '2018') | (data_mod['CMPLNT_FR_DT'] == '2019') |
                         (data_mod['CMPLNT_FR_DT'] == '2020') | (data_mod['CMPLNT_FR_DT'] == '2021')]
+'''
 #city = (['ALL', 'BRONX', 'BROOKLYN', 'MANHATTAN', 'QUEENS', 'STATEN ISLAND']), 
+
 def test_histogram():
-    plot1 = PF.plot_Factory.create('histogram_plot', data_mod)
+    plot1 = PF.plot_Factory.create('map_plot', data_mod)
     plot1.draw('month')
+
 def test_linechart():
     plot2 = PF.plot_Factory.create('line_plot', data_mod)
     plot2.draw('month')
+
 '''
 def test_map():
     plot2 = PF.plot_Factory.create('map_plot', data_mod)
