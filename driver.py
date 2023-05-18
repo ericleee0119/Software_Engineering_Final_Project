ba9b13fd0823f1e1d8d2fd66ea694ee1b01a7381
ericleee0119 <50438750+ericleee0119@users.noreply.github.com>
2023-05-18 16:26:41 -0400

    Update

--- driver.py
+++ driver.py
@@ -18,18 +18,21 @@ import finalproject_UI
 #data = None
 
 def Data_preprocess(data):
+    
     data_mod = data
-    data_mod['CMPLNT_FR_TM'] = data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
+        
+    '''data_mod['CMPLNT_FR_TM'] = data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
     data_mod['month'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[0]
-    data_mod['CMPLNT_FR_DT'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[2]
+    data_mod['CMPLNT_FR_DT'] = data_mod['CMPLNT_FR_DT'].str.split('/').str[2]'''
 
 
     #print(data_mod)
-    data_mod = data_mod.loc[(data_mod['CMPLNT_FR_DT'] == '2012') | (data_mod['CMPLNT_FR_DT'] == '2013') |
+    '''data_mod = data_mod.loc[(data_mod['CMPLNT_FR_DT'] == '2012') | (data_mod['CMPLNT_FR_DT'] == '2013') |
                             (data_mod['CMPLNT_FR_DT'] == '2014') | (data_mod['CMPLNT_FR_DT'] == '2015') |
                             (data_mod['CMPLNT_FR_DT'] == '2016') | (data_mod['CMPLNT_FR_DT'] == '2017') |
                             (data_mod['CMPLNT_FR_DT'] == '2018') | (data_mod['CMPLNT_FR_DT'] == '2019') |
-                            (data_mod['CMPLNT_FR_DT'] == '2020') | (data_mod['CMPLNT_FR_DT'] == '2021')]
+                            (data_mod['CMPLNT_FR_DT'] == '2020') | (data_mod['CMPLNT_FR_DT'] == '2021')]'''
+    
     return data_mod
 
 def File_input():
@@ -47,8 +50,9 @@ def Map_plot():
     # chosen_city = selected_options[0]
     # chosen_plot = selected_options[1]
     # chosen_col = selected_options[2]
-    mapPlot = map.MapPlot(data)
-    #mapPlot.create(data)
+    
+    mapPlot = PF.plot_Factory.create("map_plot", data)
+    # mapPlot.create(data)
     mapPlot.draw("abc")
 
 def Data_plot():
--- map.py
+++ map.py
@@ -25,79 +25,86 @@ class MapPlot(plot):
         slider = widgets.IntSlider(value = 30, min = 5, max = 50, step = 5, 
                               description = "clusters num", continuous_update=False, readout = True)
         def __init__(self, map_data):
-            
-            
             self.map_data_mod = map_data
             self.map_data_mod['CMPLNT_FR_TM'] = self.map_data_mod['CMPLNT_FR_TM'].str.split(':').str[0]
             self.map_data_mod['CMPLNT_FR_DT'] = self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] + '/' + self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[2]
 
-            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[1] == '2018')]
-            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] == '03')]     
-            
-            print(self.map_data_mod)
+            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[1] == '2021')]
+            self.map_data_mod = self.map_data_mod.loc[(self.map_data_mod['CMPLNT_FR_DT'].str.split('/').str[0] == '08')]     
+
             
         
         def draw(self, col):
-            def setbuttonclick(b, city="", slider = "", pin="", api=""):
-                city = self.city
-                slider = self.slider
-                pin = self.pin
-                api = self.api
-                global buttonClick
-                if api == "":
-                    buttonClick = True
-                    return
-              
-                curr_data = self.map_data_mod
-                first_place = "New York, USA"
-                if city != 'ALL':
-                    curr_data = curr_data.loc[curr_data['BORO_NM'] == city]
-                    first_place = city + ", USA"
-                min_num = len(curr_data)
-                min_num = min(1000, len(curr_data))
-                curr_data = curr_data.sample(n = min_num, replace = False)
-                coords = np.array(curr_data[['Latitude', 'Longitude']], dtype='float64')
-                mask = np.any(np.isnan(coords) | np.equal(coords, 0), axis=1)
-                coords[~mask]
-                for i in range(len(coords)):
-                    if math.isnan (coords[i][1]):
-                        print(i)
-                        print(coords[i])
-                        print('have null')
-                up = slider
-                Ks = range(1, up)
-                algorithmFactory = AlgorithmFactory.AlgorithmFactory.create("kmean")
-                
-                kmean = algorithmFactory.calculate(up, Ks, coords)
-                lat_list = []
-                long_list = []
+            next = True
+            while True:
+                if next == False:
+                    continue
+                next = False
+                print()
+                def setbuttonclick(b, city="", slider = "", pin="", api=""):
+                    print("check")
+                    city = self.city
+                    slider = self.slider
+                    pin = self.pin
+                    api = self.api
+                    global buttonClick
+                    if api == "":
+                        buttonClick = True
+                        return
 
-                for i in range(len(kmean)):
-                    lat_list.append(kmean[up - 2].cluster_centers_[i][0])
-                    long_list.append(kmean[up - 2].cluster_centers_[i][1])
+                    curr_data = self.map_data_mod
+                    first_place = "New York, USA"
+                    if city != 'ALL':
+                        curr_data = curr_data.loc[curr_data['BORO_NM'] == city]
+                        first_place = city + ", USA"
+                    min_num = len(curr_data)
+                    min_num = min(1000, len(curr_data))
+                    curr_data = curr_data.sample(n = min_num, replace = False)
+                    coords = np.array(curr_data[['Latitude', 'Longitude']], dtype='float64')
+                    mask = np.any(np.isnan(coords) | np.equal(coords, 0), axis=1)
+                    coords[~mask]
+                    for i in range(len(coords)):
+                        if math.isnan (coords[i][1]):
+                            print(i)
+                            print(coords[i])
+                            print('have null')
+                    up = slider
+                    Ks = range(1, up)
+                    algorithmFactory = AlgorithmFactory.AlgorithmFactory.create("kmean")
 
-                apikey = str(api)
-                gmap = gmplot.GoogleMapPlotter.from_geocode("New York, USA", apikey=apikey)
+                    kmean = algorithmFactory.calculate(up, Ks, coords)
+                    lat_list = []
+                    long_list = []
 
-                if pin == "HEAT":
-                    gmap.heatmap( lat_list, long_list, radius = 15)
-                else:
-                    gmap.scatter(lat_list, long_list, "cornflowerblue")
-                gmap.draw('map.html')
+                    for i in range(len(kmean)):
+                        lat_list.append(kmean[up - 2].cluster_centers_[i][0])
+                        long_list.append(kmean[up - 2].cluster_centers_[i][1])
 
-                filename = 'file:///'+os.getcwd()+'/' + 'map.html'
-                webbrowser.open_new_tab(filename)
-                buttonClick = False
-            @interact
-            def map_plot(city = (['ALL', 'BRONX', 'BROOKLYN', 'MANHATTAN', 'QUEENS', 'STATEN ISLAND']), 
-                                      pin = (['HEAT', 'SCATTER']), 
-                                       slider = self.slider, api = api):
-                self.city = city
-                self.slider = slider
-                self.pin = pin
-                self.api = api
-                display(button)
+                    apikey = str(api)
+                    gmap = gmplot.GoogleMapPlotter.from_geocode("New York, USA", apikey=apikey)
+
+                    if pin == "HEAT":
+                        gmap.heatmap( lat_list, long_list, radius = 15)
+                    else:
+                        gmap.scatter(lat_list, long_list, "cornflowerblue")
+                    gmap.draw('map.html')
+
+                    filename = 'file:///'+os.getcwd()+'/' + 'map.html'
+                    webbrowser.open_new_tab(filename)
+                    buttonClick = False
+                    next = True
+                #@interact
+                @interact
+                def map_plot(city = (['ALL', 'BRONX', 'BROOKLYN', 'MANHATTAN', 'QUEENS', 'STATEN ISLAND']), 
+                                          pin = (['HEAT', 'SCATTER']), 
+                                           slider = self.slider, api = api):
+                    self.city = city
+                    self.slider = slider
+                    self.pin = pin
+                    self.api = api
+                    display(button)
             button.on_click(functools.partial(setbuttonclick, city=self.city, slider = self.slider, pin=self.pin, api=self.api))
+
                 
                 
                 
